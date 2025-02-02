f9de44c978e7bb0a0f1488e583550bbf38974dbc
Lucas Pope <lpopedv@proton.me>
2025-02-02 09:36:56 -0300

    feat: add Users.Get in users delegate

--- lib/elixir_bank/users.ex
+++ lib/elixir_bank/users.ex
@@ -1,5 +1,7 @@
 defmodule ElixirBank.Users do
   alias ElixirBank.Users.Create
+  alias ElixirBank.Users.Get
 
  defdelegate create(params), to: Create, as: :call
+ defdelegate get(params), to: Get, as: :call
 end
