ee0cea4e1043e47f519ac1d3446e1aec984b8899
Lucas Pope <lpopedv@proton.me>
2025-02-18 11:42:57 -0300

    feat: add viacep client in create

--- lib/elixir_bank/users/create.ex
+++ lib/elixir_bank/users/create.ex
@@ -1,10 +1,13 @@
 defmodule ElixirBank.Users.Create do
+  alias ElixirBank.ViaCep.Client, as: ViaCepClient
   alias ElixirBank.Users.User
   alias ElixirBank.Repo
 
-  def call(params) do
-    params
-    |> User.changeset()
-    |> Repo.insert()
+  def call(%{"cep" => cep} = params) do
+    with {:ok, _result} <- ViaCepClient.call(cep) do
+      params
+      |> User.changeset()
+      |> Repo.insert()
+    end
   end
 end
